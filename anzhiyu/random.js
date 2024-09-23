ffae8bac7134d8d95aad83b6480391b2639333eb
binghe <binghe@binghedeMacBook-Pro.local>
2024-09-23 14:53:15 +0800

    Site updated: 2024-09-23 14:53:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/hello-world/","/hello-world1/","/hello-world2/"];function toRandomPost(){
+var posts=["posts/hello-world1/","posts/hello-world2/","posts/（干货）副业卖考研虚拟资料，月入过万！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
