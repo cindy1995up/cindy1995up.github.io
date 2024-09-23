3e7df2c671ec68b6bd4d536b520627d0bfb96ce8
binghe <binghe@binghedeMacBook-Pro.local>
2024-09-23 15:25:08 +0800

    Site updated: 2024-09-23 15:25:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/hello-world1/","posts/hello-world2/","posts/（干货）副业卖考研虚拟资料，月入过万！/"];function toRandomPost(){
+var posts=["posts/（干货）副业卖考研虚拟资料，月入过万！/","posts/(干货)抖音代打电话项目，日入￥500+/","posts/（干货）某红书3大热门领域爆款创作中不为人知的套路/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
