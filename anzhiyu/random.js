5c1ab8b8fa9688835b8a019945c01b7f20edf621
binghe <binghe@binghedeMacBook-Pro.local>
2024-10-19 16:50:25 +0800

    Site updated: 2024-10-19 16:50:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["douyindaidadianhua.html","shengcai-dongyangyadianshang-shopee-bentu-dian.html","kaoyanxuniziliao.html","shengcai-xianxiashitidian-huodongcehua.html","xiaohongshu-xianyu.html","xiaohongshusandaremen.html"];function toRandomPost(){
+var posts=["douyindaidadianhua.html","kaoyanxuniziliao.html","shengcai-dongyangyadianshang-shopee-bentu-dian.html","shengcai-xianxiashitidian-huodongcehua.html","xiaohongshu-xianyu.html","xiaohongshusandaremen.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
