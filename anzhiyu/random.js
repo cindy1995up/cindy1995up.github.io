c399ef75e316a293879ab1832d40e98cfe41b20b
binghe <binghe@binghedeMacBook-Pro.local>
2024-10-19 16:21:55 +0800

    Site updated: 2024-10-19 16:21:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/【副业】“小红书种草、闲鱼店开花”—单账号月入1w+/","posts/【副业】卖考研虚拟资料，月入过万！/","posts/【副业】抖音代打电话项目，日入￥500+/","posts/【副业】小红书3大热门领域爆款创作中不为人知的套路/"];function toRandomPost(){
+var posts=["douyindaidadianhua.html","shengcai-dongyangyadianshang-shopee-bentu-dian.html","kaoyanxuniziliao.html","shengcai-xianxiashitidian-huodongcehua.html","xiaohongshu-xianyu.html","xiaohongshusandaremen.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
